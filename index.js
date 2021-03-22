30dd4a80f6efe57a51ddce5b3240679063cfc367
nwraimo <raimo.nick9@gmail.com>
2021-03-22 11:15:22 -0700

    task 1 complete

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(inning){
+    
 }
 
 
